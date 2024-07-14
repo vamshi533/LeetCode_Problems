fe2c5aa6000902667c0ff39d10c3b95ae058c01a
vamshi533 <107039412+vamshi533@users.noreply.github.com>
2024-07-14 19:27:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (26.25%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -13,28 +13,53 @@
 //     }
 // };
 
+// class Solution{
+//     public:
+//     int singleNumber(vector<int>nums)
+//     {
+//         int n=nums.size();
+//         sort(nums.begin(),nums.end());
+//         int res=0;
+//         for(int i=0;i<32;i++)
+//         {
+//             int c=0;
+//             for(int j=0;j<n;j++)
+//             {
+//                 if(nums[j] & (1<<i))
+//                 {
+//                     c++;
+//                 }
+//             }
+//             if(c%3!=0)
+//             {
+//                 res=res+(1<<i);
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
 class Solution{
     public:
-    int singleNumber(vector<int>nums)
+    int singleNumber(vector<int>&nums)
     {
-        int n=nums.size();
         sort(nums.begin(),nums.end());
-        int res=0;
-        for(int i=0;i<32;i++)
+        int n=nums.size();
+        for(int i=1;i<n;)
         {
-            int c=0;
-            for(int j=0;j<n;j++)
+            if(nums[i]!=nums[i-1])
             {
-                if(nums[j] & (1<<i))
-                {
-                    c++;
-                }
+                return nums[i-1];
             }
-            if(c%3!=0)
-            {
-                res=res+(1<<i);
+            else{
+                i=i+3;
             }
+            // if(i>n)
+            // {
+            //     return nums[n-1];
+            // }
         }
-        return res;
+        return nums[n-1];
     }
 };

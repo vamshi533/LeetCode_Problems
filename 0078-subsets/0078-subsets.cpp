86bf38fb72b6437d9533c1ae8cfc32d9a8bd0324
vamshi533 <107039412+vamshi533@users.noreply.github.com>
2024-07-14 16:35:12 +0530

    Time: 0 ms (100%), Space: 8.7 MB (40.25%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,43 @@
+// class Solution {
+// public:
+//     std::vector<std::vector<int>> subsets(std::vector<int>& nums) {
+//         std::vector<std::vector<int>> res;
+//         std::vector<int> op;
+//         solve(nums, 0, op, res);
+//         return res;
+//     }
+    
+//     void solve(std::vector<int>& nums, int start, std::vector<int>& op, std::vector<std::vector<int>>& res) {
+//         if (nums.size() == start) {
+//             res.push_back(op);
+//             return;
+//         }
+        
+//         solve(nums, start + 1, op, res);
+//         op.push_back(nums[start]);
+//         solve(nums, start + 1, op, res);
+//         op.pop_back();
+//     }
+// };
+
+class Solution{
+    public:
+    vector<vector<int>>subsets(vector<int>&nums)
+    {
+        int n=nums.size();
+        vector<vector<int>>ans;
+        for(int i=0;i<(1<<n);i++)
+        {
+            vector<int>temp;
+            for(int j=0;j<n;j++)
+            {
+                if(i&(1<<j))
+                {
+                    temp.push_back(nums[j]);
+                }
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
